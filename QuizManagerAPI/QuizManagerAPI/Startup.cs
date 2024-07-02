3987013313a429d592f1d617229d830ddc51dc9d
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-07-02 16:56:05 +0100

    edit cors config

--- QuizManagerAPI/QuizManagerAPI/Startup.cs
+++ QuizManagerAPI/QuizManagerAPI/Startup.cs
@@ -24,51 +24,29 @@ namespace QuizManagerApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Enable CORS
-            services.AddCors(c =>
+            services.AddCors(options =>
             {
-                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                options.AddPolicy("AllowSpecificOrigin",
+                    builder => builder.WithOrigins("http://localhost:3000")
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod());
             });
-
-
-            //JSON Serializer
-            services.AddControllersWithViews().AddNewtonsoftJson(options =>
-            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
-                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
-                = new DefaultContractResolver());
 
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //Enable CORS
-            app.UseCors("AllowAll");//(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            app.UseCors("AllowSpecificOrigin");
 
             app.UseRouting();
-
-            //app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
-
-            //app.UseStaticFiles(new StaticFileOptions
-            //{
-            //    FileProvider = new PhysicalFileProvider(
-            //       Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
-            //    RequestPath = "/Photos"
-            //});
         }
+
     }
 }
