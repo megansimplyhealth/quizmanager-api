99d898db4b841320972d52c9b6a92fd560139311
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-07-03 08:44:27 +0100

    deleted startup moved cors to program

--- QuizManagerAPI/QuizManagerAPI/Program.cs
+++ QuizManagerAPI/QuizManagerAPI/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -19,6 +20,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(builder => builder
+                 .AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+//app.UseMvc();
 
 app.UseHttpsRedirection();
 
