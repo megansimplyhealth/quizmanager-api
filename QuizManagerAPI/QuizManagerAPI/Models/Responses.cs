5943dc1e597bc4b94fcfcf33c5fa302bc79210fd
Megan Knaresborough <Megan.Knaresborough@simplyhealth.co.uk>
2024-07-04 12:23:52 +0100

    edit reponse working leaderboard

--- QuizManagerAPI/QuizManagerAPI/Models/Responses.cs
+++ QuizManagerAPI/QuizManagerAPI/Models/Responses.cs
@@ -4,8 +4,8 @@
     {
         public int ResponseId { get; set; }
         public string ResponseName { get; set; }
-        public DateTime ResponseDate { get; set; }
-        public DateTime ResponseTime { get; set; }
+        public string ResponseDate { get; set; }
+        public string ResponseTime { get; set; }
         public int ResponseScore { get; set; }
     }
 }
